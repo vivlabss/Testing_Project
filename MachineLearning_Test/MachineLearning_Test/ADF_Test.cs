fbbe60422f05a8e980f2d144ea46a5eeaba7e3b3
DESKTOP-HVPPI1O\com <youlive789@gmail.com>
2017-04-02 19:37:43 +0900

    다중회귀 진행해야함. 계산내용에 차이가 많음

---
+++ MachineLearning_Test/MachineLearning_Test/ADF_Test.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accord;
+using Accord.MachineLearning;
+using Accord.Statistics.Models.Regression.Linear;
+
+namespace MachineLearning_Test
+{
+    class ADF_Test
+    {
+        // 참고자료 : http://reliawiki.org/index.php/Simple_Linear_Regression_Analysis
+        static void Main(string[] args)
+        {
+            double[] raw_data = { 5, 4, 7, 5, 3, 2, 1, 5, 7, 5 }; // 향후 데이터를 입력받음
+            double[] diff_raw_data = new double[raw_data.Length - 1];
+            double[] lag_raw_data = new double[raw_data.Length - 1];
+            double[] diff_lag_raw_data = new double[raw_data.Length - 2];
+
+            double[] output_diff = new double[raw_data.Length - 2];
+            double[] input_lag = new double[raw_data.Length - 2];
+            double[] input_diff_lag = new double[raw_data.Length - 2];
+            double[] predict_lag = new double[raw_data.Length - 2];
+            double[] predict_diff_lag = new double[raw_data.Length - 2];
+            double[] predicted = new double[raw_data.Length - 2];
+            double[][] inputs = new double[raw_data.Length - 2][];
+
+            // diff
+            for (int i = 0; i < raw_data.Length-1; i++) diff_raw_data[i] = raw_data[i + 1] - raw_data[i];
+            // Lag
+            for (int i = 0; i < raw_data.Length - 1; i++) lag_raw_data[i] = raw_data[i];
+            // diff_lag
+            for (int i = 0; i < raw_data.Length - 2; i++) diff_lag_raw_data[i] = lag_raw_data[i + 1] - lag_raw_data[i];
+
+            // 데이터 세팅
+            for (int i = 0; i < raw_data.Length - 2; i++) output_diff[i] = diff_raw_data[i + 1];
+            for (int i = 0; i < raw_data.Length - 2; i++) input_lag[i] = lag_raw_data[i + 1];
+            for (int i = 0; i < raw_data.Length - 2; i++) input_diff_lag[i] = diff_lag_raw_data[i];
+
+            // 다중 선형 회귀
+            OrdinaryLeastSquares olss = new OrdinaryLeastSquares() { UseIntercept = true };
+            for (int i = 0; i < raw_data.Length - 2; i++) inputs[i] = new double[] { input_lag[i] , input_diff_lag[i]};
+            MultipleLinearRegression regression = olss.Learn(inputs, output_diff);
+            for (int i = 0; i < raw_data.Length - 2; i++) predicted[i] = regression.Transform(inputs[i]);
+            Console.WriteLine(regression.Weights[0] + "\t" + regression.Weights[1]);
+            Console.WriteLine(Calc_T(inputs, output_diff, predicted, regression)[0] + "\t" + Calc_T(inputs, output_diff, predicted, regression)[1]);
+        }
+
+        static double[] Calc_T(double[][] input, double[] output , double[] predict, MultipleLinearRegression regression)
+        {
+            double error = 0;
+            double SSE = 0;
+            double SSE2 = 0;
+            double xMean1 = 0, xMean2 = 0;
+            double temp1 = 0 , temp2 = 0;
+            double[] tScore = new double[2];
+
+            for (int i = 0; i < output.Length - 2; i++) error += (output[i] - regression.Transform(input)[i]) * (output[i] - regression.Transform(input)[i]);
+
+            for (int i = 0; i < output.Length - 2; i++)
+            {
+                temp1 += input[i][0];
+                temp2 += input[i][1];
+            }
+            xMean1 = temp1 / output.Length; temp1 = 0;
+            xMean2 = temp2 / output.Length; temp2 = 0;
+            for (int i = 0; i < output.Length - 2; i++)
+            {
+                SSE += (input[i][0] - xMean1) * (input[i][0] - xMean1);
+                SSE2 += (input[i][1] - xMean2) * (input[i][1] - xMean2);
+            }
+            Console.WriteLine((SSE / output.Length - 2) + "\t" + (SSE2 / output.Length - 2));
+            tScore[0] = Math.Sqrt((error / output.Length - 2) / SSE);
+            tScore[1] = Math.Sqrt((error / output.Length - 2) / SSE2);
+
+            return tScore;
+        }
+    }
+}
