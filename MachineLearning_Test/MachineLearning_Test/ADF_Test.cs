8e8625f253deb543d412dea3fb9118a0d748a578
GeonWoo Kim <youlive123@nate.com>
2017-04-03 14:32:40 +0900

    ADF TEST 알고리즘 준완성 - 추가할 사항 1 : ADF 지수 판단 기준 - 추가할 사항 2 :  SQL데이터를 배열로 받아와서 실행

--- MachineLearning_Test/MachineLearning_Test/ADF_Test.cs
+++ MachineLearning_Test/MachineLearning_Test/ADF_Test.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Accord;
 using Accord.MachineLearning;
 using Accord.Statistics.Models.Regression.Linear;
+
+using System.Data;
+using System.Data.SqlClient;
 
 namespace MachineLearning_Test
 {
@@ -15,6 +19,7 @@ namespace MachineLearning_Test
         // http://www.stat.yale.edu/Courses/1997-98/101/linmult.htm
         // https://www3.nd.edu/~rwilliam/stats2/l02.pdf
         // http://dept.stat.lsa.umich.edu/~kshedden/Courses/Stat401/Notes/401-multreg.pdf
+        // Multiple Linear Regression Testing - http://hspm.sph.sc.edu/Courses/J716/pdf/716-3%20Multiple%20Regression.pdf
 
         static void Main(string[] args)
         {
@@ -55,56 +60,37 @@ namespace MachineLearning_Test
         static double[] Calc_T(double[][] input, double[] output , double[] predict, MultipleLinearRegression regression)
         {
             double std1 = 0 , std2 = 0, error=0;
-            double xMean1 = 0, xMean2 = 0, yMean = 0;
+            double xMean1 = 0, xMean2 = 0;
             double temp1 = 0 , temp2 = 0;
             double[] tScore = new double[2];
 
-            //for (int i = 0; i < output.Length; i++) error += (output[i] - regression.Transform(input)[i]) * (output[i] - regression.Transform(input)[i]);
-            //error /= output.Length;
-            //error = Math.Sqrt(error);
-            for (int i = 0; i < output.Length; i++) yMean += output[i];
-            yMean /= output.Length;
-            for (int i = 0; i < output.Length; i++) error += (output[i] - yMean) * (output[i] - yMean);
-            error /= output.Length;
+            for (int i = 0; i < output.Length; i++) error += (output[i] - regression.Transform(input)[i]) * (output[i] - regression.Transform(input)[i]);
+            error /= output.Length -3;
+            error = Math.Sqrt(error);
 
             for (int i = 0; i < output.Length; i++) xMean1 += input[i][0];
             xMean1 /= output.Length;
             for (int i = 0; i < output.Length; i++) xMean2 += input[i][1];
             xMean2 /= output.Length;
 
-            for (int i = 0; i < output.Length; i++) std1 += (input[i][0] - xMean1) * (input[i][0] - xMean1);
-            std1 /= output.Length;
-            std1 = Math.Sqrt(std1);
-            std1 = (error / (Math.Sqrt(output.Length-1) * std1));
-            for (int i = 0; i < output.Length; i++) std2 += (input[i][1] - xMean2) * (input[i][1] - xMean2);
-            std2 /= output.Length;
-            std2 = Math.Sqrt(std2);
-            std2 = (error / (Math.Sqrt(output.Length-1) * std2));
+            for (int i = 0; i < output.Length; i++) std1 += (input[i][0] - xMean1) * (input[i][1] - xMean2);
+            std1 = Math.Pow(std1, 2);
+            for (int i = 0; i < output.Length; i++) temp1 += (input[i][1] - xMean2) * (input[i][1] - xMean2);
+            std1 = std1 / temp1; temp1 = 0;
+            for (int i = 0; i < output.Length; i++) temp1 += (input[i][0] - xMean1) * (input[i][0] - xMean1);
+            std1 = temp1 - std1;
+            std1 =error / Math.Sqrt(std1);
 
-            Console.WriteLine(std1 + "\t" + std2);
+            for (int i = 0; i < output.Length; i++) std2 += (input[i][0] - xMean1) * (input[i][1] - xMean2);
+            std2 = Math.Pow(std2, 2);
+            for (int i = 0; i < output.Length; i++) temp2 += (input[i][0] - xMean1) * (input[i][0] - xMean1);
+            std2 = std2 / temp2; temp2 = 0;
+            for (int i = 0; i < output.Length; i++) temp2 += (input[i][1] - xMean2) * (input[i][1] - xMean2);
+            std2 = temp2 - std2;
+            std2 = error / Math.Sqrt(std2);
 
             tScore[0] = regression.Weights[0] / std1;
             tScore[1] = regression.Weights[1] / std2;
-
-
-            // 잘못 계산한 부분이 많음
-            //for (int i = 0; i < output.Length - 2; i++) error += (output[i] - regression.Transform(input)[i]) * (output[i] - regression.Transform(input)[i]);
-
-            //for (int i = 0; i < output.Length - 2; i++)
-            //{
-            //    temp1 += input[i][0];
-            //    temp2 += input[i][1];
-            //}
-            //xMean1 = temp1 / output.Length; temp1 = 0;
-            //xMean2 = temp2 / output.Length; temp2 = 0;
-            //for (int i = 0; i < output.Length - 2; i++)
-            //{
-            //    SSE += (input[i][0] - xMean1) * (input[i][0] - xMean1);
-            //    SSE2 += (input[i][1] - xMean2) * (input[i][1] - xMean2);
-            //}
-            //Console.WriteLine((SSE / output.Length - 2) + "\t" + (SSE2 / output.Length - 2));
-            //tScore[0] = Math.Sqrt((error / output.Length - 2) / SSE);
-            //tScore[1] = Math.Sqrt((error / output.Length - 2) / SSE2);
 
             return tScore;
         }
