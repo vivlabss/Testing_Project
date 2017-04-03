f349297347ab485e6a364c9c32aa915a3330a187
GeonWoo Kim <youlive123@nate.com>
2017-04-03 12:55:34 +0900

    Multiple Linear Regression T-Test 점수 계산 수정중 KNN 학습 데이터 100개 진행 => 여전히 점 검출 불가

--- MachineLearning_Test/MachineLearning_Test/ADF_Test.cs
+++ MachineLearning_Test/MachineLearning_Test/ADF_Test.cs
@@ -12,6 +12,10 @@ namespace MachineLearning_Test
     class ADF_Test
     {
         // 참고자료 : http://reliawiki.org/index.php/Simple_Linear_Regression_Analysis
+        // http://www.stat.yale.edu/Courses/1997-98/101/linmult.htm
+        // https://www3.nd.edu/~rwilliam/stats2/l02.pdf
+        // http://dept.stat.lsa.umich.edu/~kshedden/Courses/Stat401/Notes/401-multreg.pdf
+
         static void Main(string[] args)
         {
             double[] raw_data = { 5, 4, 7, 5, 3, 2, 1, 5, 7, 5 }; // 향후 데이터를 입력받음
@@ -50,30 +54,57 @@ namespace MachineLearning_Test
 
         static double[] Calc_T(double[][] input, double[] output , double[] predict, MultipleLinearRegression regression)
         {
-            double error = 0;
-            double SSE = 0;
-            double SSE2 = 0;
-            double xMean1 = 0, xMean2 = 0;
+            double std1 = 0 , std2 = 0, error=0;
+            double xMean1 = 0, xMean2 = 0, yMean = 0;
             double temp1 = 0 , temp2 = 0;
             double[] tScore = new double[2];
 
-            for (int i = 0; i < output.Length - 2; i++) error += (output[i] - regression.Transform(input)[i]) * (output[i] - regression.Transform(input)[i]);
+            //for (int i = 0; i < output.Length; i++) error += (output[i] - regression.Transform(input)[i]) * (output[i] - regression.Transform(input)[i]);
+            //error /= output.Length;
+            //error = Math.Sqrt(error);
+            for (int i = 0; i < output.Length; i++) yMean += output[i];
+            yMean /= output.Length;
+            for (int i = 0; i < output.Length; i++) error += (output[i] - yMean) * (output[i] - yMean);
+            error /= output.Length;
 
-            for (int i = 0; i < output.Length - 2; i++)
-            {
-                temp1 += input[i][0];
-                temp2 += input[i][1];
-            }
-            xMean1 = temp1 / output.Length; temp1 = 0;
-            xMean2 = temp2 / output.Length; temp2 = 0;
-            for (int i = 0; i < output.Length - 2; i++)
-            {
-                SSE += (input[i][0] - xMean1) * (input[i][0] - xMean1);
-                SSE2 += (input[i][1] - xMean2) * (input[i][1] - xMean2);
-            }
-            Console.WriteLine((SSE / output.Length - 2) + "\t" + (SSE2 / output.Length - 2));
-            tScore[0] = Math.Sqrt((error / output.Length - 2) / SSE);
-            tScore[1] = Math.Sqrt((error / output.Length - 2) / SSE2);
+            for (int i = 0; i < output.Length; i++) xMean1 += input[i][0];
+            xMean1 /= output.Length;
+            for (int i = 0; i < output.Length; i++) xMean2 += input[i][1];
+            xMean2 /= output.Length;
+
+            for (int i = 0; i < output.Length; i++) std1 += (input[i][0] - xMean1) * (input[i][0] - xMean1);
+            std1 /= output.Length;
+            std1 = Math.Sqrt(std1);
+            std1 = (error / (Math.Sqrt(output.Length-1) * std1));
+            for (int i = 0; i < output.Length; i++) std2 += (input[i][1] - xMean2) * (input[i][1] - xMean2);
+            std2 /= output.Length;
+            std2 = Math.Sqrt(std2);
+            std2 = (error / (Math.Sqrt(output.Length-1) * std2));
+
+            Console.WriteLine(std1 + "\t" + std2);
+
+            tScore[0] = regression.Weights[0] / std1;
+            tScore[1] = regression.Weights[1] / std2;
+
+
+            // 잘못 계산한 부분이 많음
+            //for (int i = 0; i < output.Length - 2; i++) error += (output[i] - regression.Transform(input)[i]) * (output[i] - regression.Transform(input)[i]);
+
+            //for (int i = 0; i < output.Length - 2; i++)
+            //{
+            //    temp1 += input[i][0];
+            //    temp2 += input[i][1];
+            //}
+            //xMean1 = temp1 / output.Length; temp1 = 0;
+            //xMean2 = temp2 / output.Length; temp2 = 0;
+            //for (int i = 0; i < output.Length - 2; i++)
+            //{
+            //    SSE += (input[i][0] - xMean1) * (input[i][0] - xMean1);
+            //    SSE2 += (input[i][1] - xMean2) * (input[i][1] - xMean2);
+            //}
+            //Console.WriteLine((SSE / output.Length - 2) + "\t" + (SSE2 / output.Length - 2));
+            //tScore[0] = Math.Sqrt((error / output.Length - 2) / SSE);
+            //tScore[1] = Math.Sqrt((error / output.Length - 2) / SSE2);
 
             return tScore;
         }
--- MachineLearning_Test/MachineLearning_Test/KNN_IMAGE.cs
+++ MachineLearning_Test/MachineLearning_Test/KNN_IMAGE.cs
@@ -19,12 +19,13 @@ namespace MachineLearning_Test
             byte[] temp01;
             byte[] test;
             double[] temp02;
-            double[][] inputs = new double[40][];
-            int[] outputs = new int[40];
+            double[][] inputs = new double[100][];
+            int[] outputs = new int[100];
 
-            for (int i = 0; i < 20; i++)
+            // 상대경로 처리를 진행 => bin/Debug/Data 폴더가 필요함 !!
+            for (int i = 0; i < 50; i++)
             {
-                Image image = Image.FromFile(@"C:\Users\youli\Desktop\프로그래밍\C#\Testing_Project\MachineLearning_Test\MachineLearning_Test\Data\train\false" + (i + 1) + ".bmp");
+                Image image = Image.FromFile(System.IO.Directory.GetCurrentDirectory() + @"\Data\train\false" + (i + 1) + ".bmp");
                 temp01 = imageToByteArray(image);
                 List<double> temp_1 = new List<double>();
                 double[] temp_2;
@@ -33,34 +34,34 @@ namespace MachineLearning_Test
                 inputs[i] = temp_2;
             }
 
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 50; i++)
             {
-                Image image = Image.FromFile(@"C:\Users\youli\Desktop\프로그래밍\C#\Testing_Project\MachineLearning_Test\MachineLearning_Test\Data\train\true" + (i + 1) + ".bmp");
+                Image image = Image.FromFile(System.IO.Directory.GetCurrentDirectory() + @"\Data\train\true" + (i + 1) + ".bmp");
                 temp01 = imageToByteArray(image);
                 List<double> temp_1 = new List<double>();
                 double[] temp_2;
                 temp01.ToList<byte>().ForEach(b => temp_1.Add(Convert.ToDouble(b)));
                 temp_2 = temp_1.ToArray<double>();
-                inputs[i + 20] = temp_2;
+                inputs[i + 50] = temp_2;
             }
 
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 50; i++)
             {
                 outputs[i] = 0;
-                outputs[i + 20] = 1;
+                outputs[i + 50] = 1;
             }
             //C:\Users\com\Desktop\Programming\images\train￦10.jpg
             KNearestNeighbors knn = new KNearestNeighbors(k: 3, classes: 2, inputs: inputs, outputs: outputs);
 
             for (int i = 0; i < 10; i++)
             {
-                Image tester = Image.FromFile(@"C:\Users\youli\Desktop\프로그래밍\C#\Testing_Project\MachineLearning_Test\MachineLearning_Test\Data\test\" + (i + 1) + ".bmp");
+                Image tester = Image.FromFile(System.IO.Directory.GetCurrentDirectory() + @"\Data\test\" + (i + 1) + ".bmp");
                 temp01 = imageToByteArray(tester);
                 List<double> temp_11 = new List<double>();
                 double[] temp_22;
                 temp01.ToList<byte>().ForEach(b => temp_11.Add(Convert.ToDouble(b)));
                 temp_22 = temp_11.ToArray<double>();
-                //Console.WriteLine(knn.Compute(temp_22));
+                Console.WriteLine(knn.Compute(temp_22));
             }
         }
 
