23bfc5a7efa300549120e0ff894f74cd65a202fd
GeonWoo Kim <youlive123@nate.com>
2017-04-12 09:37:20 +0900

    SQL 실행 함수 추가

--- MachineLearning_Test/MachineLearning_Test/HalfLife.cs
+++ MachineLearning_Test/MachineLearning_Test/HalfLife.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 using Accord;
 using Accord.MachineLearning;
@@ -15,143 +16,7 @@ namespace MachineLearning_Test
         // 참고자료 : http://marcoagd.usuarios.rdc.puc-rio.br/revers.html
         static void Main(string[] args)
         {
-            double[] raw_data = {  2095000,
-2120000,
-2092000,
-2070000,
-2068000,
-2030000,
-2009000,
-2010000,
-2010000,
-2010000,
-2004000,
-1981000,
-1986000,
-1922000,
-1922000,
-1903000,
-1911000,
-1959000,
-1965000,
-1947000,
-1933000,
-1893000,
-1901000,
-1886000,
-1879000,
-1898000,
-1918000,
-1920000,
-1920000,
-1941000,
-1978000,
-1973000,
-1968000,
-1956000,
-1973000,
-1995000,
-1995000,
-1995000,
-1970000,
-1908000,
-1903000,
-1860000,
-1874000,
-1847000,
-1848000,
-1833000,
-1873000,
-1940000,
-1914000,
-1862000,
-1861000,
-1810000,
-1778000,
-1808000,
-1824000,
-1805000,
-1802000,
-1802000,
-1788000,
-1799000,
-1798000,
-1782000,
-1809000,
-1805000,
-1812000,
-1795000,
-1793000,
-1759000,
-1777000,
-1766000,
-1752000,
-1780000,
-1790000,
-1772000,
-1748000,
-1718000,
-1727000,
-1749000,
-1746000,
-1677000,
-1677000,
-1650000,
-1650000,
-1649000,
-1640000,
-1593000,
-1586000,
-1568000,
-1558000,
-1539000,
-1553000,
-1598000,
-1649000,
-1596000,
-1644000,
-1640000,
-1627000,
-1616000,
-1643000,
-1652000,
-1639000,
-1614000,
-1573000,
-1567000,
-1597000,
-1608000,
-1589000,
-1620000,
-1625000,
-1589000,
-1590000,
-1577000,
-1557000,
-1535000,
-1545000,
-1680000,
-1706000,
-1691000,
-1619000,
-1614000,
-1598000,
-1598000,
-1600000,
-1567000,
-1569000,
-1568000,
-1571000,
-1618000,
-1592000,
-1585000,
-1558000,
-1527000,
-1527000,
-1527000,
-1527000,
-1465000,
-        }; // 향후 데이터를 입력받음
+            double[] raw_data = SqlCommandPrepare().ToArray(); // 향후 데이터를 입력받음
             double[] lag_raw_data = new double[raw_data.Length - 1];
             double[] outputs = new double[raw_data.Length - 1];
             double lifeTime;
@@ -160,21 +25,21 @@ namespace MachineLearning_Test
             for (int i = 0; i < raw_data.Length - 1; i++) lag_raw_data[i] = raw_data[i + 1];
             for (int i = 0; i < raw_data.Length - 1; i++) outputs[i] = raw_data[i] - lag_raw_data[i];
 
-            for (int i = 0; i < raw_data.Length - 1; i++)
-            {
-                Console.Write(lag_raw_data[i] + "\t");
-            }
-            Console.WriteLine();
-            for (int i = 0; i < raw_data.Length - 1; i++)
-            {
-                Console.Write(raw_data[i] + "\t");
-            }
-            Console.WriteLine();
-            for (int i = 0; i < raw_data.Length - 1; i++)
-            {
-                Console.Write(outputs[i] + "\t");
-            }
-            Console.WriteLine();
+            //for (int i = 0; i < raw_data.Length - 1; i++)
+            //{
+            //    Console.Write(lag_raw_data[i] + "\t");
+            //}
+            //Console.WriteLine();
+            //for (int i = 0; i < raw_data.Length - 1; i++)
+            //{
+            //    Console.Write(raw_data[i] + "\t");
+            //}
+            //Console.WriteLine();
+            //for (int i = 0; i < raw_data.Length - 1; i++)
+            //{
+            //    Console.Write(outputs[i] + "\t");
+            //}
+            //Console.WriteLine();
             OrdinaryLeastSquares ols = new OrdinaryLeastSquares();
 
             SimpleLinearRegression regression = ols.Learn(lag_raw_data, outputs);
@@ -189,7 +54,41 @@ namespace MachineLearning_Test
             Console.WriteLine("Half Life : " + lifeTime);
 
         }
+
+        static List<double> SqlCommandPrepare()
+        {
+            string connectionString = @"Data Source =.\SQLEXPRESS; Initial Catalog = Stock_Data; Integrated Security = True; Connect Timeout = 15; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            List<double> priceSet = new List<double>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(null, connection);
+
+                    command.CommandText =
+                        "SELECT PRICE_CLOSE FROM SK이노베이션 ORDER BY DATE ASC";
+
+                    command.ExecuteNonQuery();
+
+                    SqlDataReader dataReader = command.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        priceSet.Add(Convert.ToDouble(dataReader["PRICE_CLOSE"].ToString()));
+                    }
+
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+
+            return priceSet;
+        }
+
     }
 
-  
+
 }
--- MachineLearning_Test/MachineLearning_Test/Hurst.cs
+++ MachineLearning_Test/MachineLearning_Test/Hurst.cs
@@ -15,7 +15,7 @@ namespace MachineLearning_Test
             // H = 0.5 이면 GBM, H = 0 이면 평균회귀, H = 1 이면 추세성향
 
 
-            double[] raw_data = { 5, 4, 7, 5, 3, 2, 1, 5, 7, 5,7,8,6,5,1,2,3,5,7,8,5,7,8,9,5,4,2,3,8,9 }; // 향후 데이터를 입력받음
+            double[] raw_data = SqlCommandPrepare().ToArray(); // 향후 데이터를 입력받음
             double[] devi_raw_data = new double[raw_data.Length];
             double[] sums_devi = new double[raw_data.Length];
             double R = 0;
@@ -46,6 +46,39 @@ namespace MachineLearning_Test
 
             hurst = Math.Log(R / std) / Math.Log(raw_data.Length);
             Console.WriteLine(hurst);
+        }
+
+        static List<double> SqlCommandPrepare()
+        {
+            string connectionString = @"Data Source =.\SQLEXPRESS; Initial Catalog = Stock_Data; Integrated Security = True; Connect Timeout = 15; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            List<double> priceSet = new List<double>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(null, connection);
+
+                    command.CommandText =
+                        "SELECT PRICE_CLOSE FROM SK이노베이션 ORDER BY DATE ASC";
+
+                    command.ExecuteNonQuery();
+
+                    SqlDataReader dataReader = command.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        priceSet.Add(Convert.ToDouble(dataReader["PRICE_CLOSE"].ToString()));
+                    }
+
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+
+            return priceSet;
         }
     }
 }
