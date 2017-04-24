f9af24df40b38f5665fad0ce49db1e0b382b3903
GeonWoo Kim <youlive123@nate.com>
2017-04-24 18:18:53 +0900

    Accord 다시 시도

--- DataHandling/DataHandling/xmlParsing.cs
+++ DataHandling/DataHandling/xmlParsing.cs
@@ -34,7 +34,7 @@ namespace DataHandling
             setting.IgnoreComments = true;
             setting.IgnoreWhitespace = true;
 
-            StreamWriter sw = new StreamWriter(@"C:\Users\youli\Desktop\특허청공모전\data\DS.csv", Encoding.Unicode);
+            StreamWriter sw = new StreamWriter(@"C:\Users\acorn\Desktop\DB\DS\result.txt");
 
             for(int idx = 0; idx < itemLength1; idx++)
             {
@@ -51,7 +51,7 @@ namespace DataHandling
             sw.Dispose();
 
 
-            sw = new StreamWriter(@"C:\Users\youli\Desktop\특허청공모전\data\PT.csv", false, Encoding.Unicode);
+            sw = new StreamWriter(@"C:\Users\acorn\Desktop\DB\PT\result.txt");
 
             for (int idx = 0; idx < itemLength2; idx++)
             {
@@ -67,7 +67,7 @@ namespace DataHandling
             sw.Close();
             sw.Dispose();
 
-            sw = new StreamWriter(@"C:\Users\youli\Desktop\특허청공모전\data\TM.csv", false, Encoding.Unicode);
+            sw = new StreamWriter(@"C:\Users\acorn\Desktop\DB\TM\result.txt");
 
             for (int idx = 0; idx < itemLength3; idx++)
             {
@@ -83,7 +83,7 @@ namespace DataHandling
             sw.Close();
             sw.Dispose();
 
-            sw = new StreamWriter(@"C:\Users\youli\Desktop\특허청공모전\data\UT.csv", false, Encoding.Unicode);
+            sw = new StreamWriter(@"C:\Users\acorn\Desktop\DB\UT\result.txt");
 
             for (int idx = 0; idx < itemLength4; idx++)
             {
